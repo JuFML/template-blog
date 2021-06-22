06362b14242dec9b97cae8ae663122ba671bc4c9
Juliana Fernandez <julianafernandezlima@hotmail.com>
2021-06-22 08:27:20 -0300

    adding post

--- template_parts/post.php
+++ template_parts/post.php
@@ -1,23 +1,27 @@
 <div class="blog-post">
+
     <div class="blog-thumb">
-        <img src= <?php echo get_template_directory_uri()."/assets/images/blog-post-01.jpg" ?> alt="">
+        <?php if(has_post_thumbnail()): ?>
+            <?php the_post_thumbnail('large', array('class'=>'post-thumb')); ?>
+        <?php endif; ?>
     </div>
+
     <div class="down-content">
-        <span>Lifestyle</span>
+        <span><?php the_category() ?></span>
         <a href="<?php the_permalink(); ?>"><h4><?php the_title() ?></h4></a>
+        <div class="post--excerpt"><?php the_excerpt(); ?></div>
         <ul class="post-info">
-        <li><a href="#"><?php the_author() ?></a></li>
+        <li><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author() ?></a></li>
         <li><a href="#"><?php echo get_the_date() ?></a></li>
-        <li><a href="#">12 Comments</a></li>
+        <li><a href="#"><?php get_comments() ?></a></li>
         </ul>
-        <p>Stand Blog is a free HTML CSS template for your CMS theme. You can easily adapt or customize it for any kind of CMS or website builder. You are allowed to use it for your business. You are NOT allowed to re-distribute the template ZIP file on any template collection site for the download purpose. <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">Contact TemplateMo</a> for more info. Thank you.</p>
+       
         <div class="post-options">
         <div class="row">
             <div class="col-6">
             <ul class="post-tags">
                 <li><i class="fa fa-tags"></i></li>
-                <li><a href="#">Beauty</a>,</li>
-                <li><a href="#">Nature</a></li>
+                <?php the_tags( '', ', ', '' ) ?>                
             </ul>
             </div>
             <div class="col-6">
@@ -30,5 +34,8 @@
         </div>
         </div>
     </div>
-    </div>
+    
+</div>
+
+
                 
