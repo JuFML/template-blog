f5123c89df8324f413efb9cf8584877ae34412d7
Juliana Fernandez <julianafernandezlima@hotmail.com>
2021-06-25 05:57:02 -0300

    Adding 404 page, category page and a default page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header() ?>
+<!-- Page Content -->
+    <!-- Banner Starts Here -->
+
+    <div class="heading-page header-text">
+      <section class="page-heading">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="text-content">
+                <h4><?php the_category(); ?></h4>
+                <h2>Página não encontrada</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+     
+    <!-- Banner Ends Here -->    
+      
+<?php get_footer() ?>
