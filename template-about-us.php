0465ffc6d0b238b7123b1c1fb02e36c036e94e30
Juliana Fernandez <julianafernandezlima@hotmail.com>
2021-06-26 18:05:14 -0300

    adding template-about-us

---
+++ template-about-us.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Template Name: About Us
+*/
+?>
+<?php get_header(); ?>
+<!-- Page Content -->
+    <!-- Banner Starts Here -->
+
+    <div class="heading-page header-text">
+      <section class="page-heading">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="text-content">
+                <h4><?php the_category(); ?></h4>
+                <h2><?php the_title() ?></h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+     
+    <!-- Banner Ends Here -->
+    
+    
+    <section class="about-us">
+      <div class="container">
+      	
+        <div class="row">
+          <div class="col-lg-12">
+            <?php the_content();  ?>
+            
+        
+        <div class="row">
+          <div class="col-lg-12">
+            <ul class="social-icons">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-behance"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        
+        
+      </div>
+    </section>
+    
+<?php get_footer() ?>
